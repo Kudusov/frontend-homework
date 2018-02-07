1f760a1030268ce4ae69c4b271eb66687341df4f
Mahmud <qudusov98@mail.ru>
2018-02-07 21:09:22 +0300

    some changes)

--- source/zip.js
+++ source/zip.js
@@ -4,9 +4,12 @@ const zip = function(...objects) {
 	let result = {}
 
 	objects.forEach(object => {
-		Object.entries(object).forEach(entry => !(entry[0] in result) ? result[entry[0]] = entry[1] : {})
+		Object.entries(object).forEach(entry => { 
+			if (!(entry[0] in result)) {
+				result[entry[0]] = entry[1]
+			}
+		})
 	})
 
 	return result
-
 }
