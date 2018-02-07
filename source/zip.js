bff41263db6fc6332dcb95bbf97f62c5228f8fa4
Mahmud <qudusov98@mail.ru>
2018-02-07 17:09:21 +0300

    some changes

--- source/zip.js
+++ source/zip.js
@@ -1,25 +1,17 @@
 'use strict';
 
-/*
-const zip = function() {
+const zip = function(...objects) {
+	let result = {}
 
-	return Object.assign(...arguments)
-
-}
-*/
-
-const zip = function() {
-	var result = {}
-
-	for ( var i = 0; i < arguments.length; i++ ) {
-		var entries = Object.entries(arguments[i])
-		for ( var j = 0; j < entries.length; j++ ) {
-			if ( !(entries[j][0] in result) ) {
-				result[entries[j][0]] = entries[j][1]
+	objects.forEach(function(object, i, objects) {
+		let entries = Object.entries(object)
+		entries.forEach(function(entry, j, entries) {
+			if (!(entry[0] in result)) {
+				result[entry[0]] = entry[1]
 			}
-		}
-	}
+		})
+	})
 
-	console.log(result)
 	return result
+
 }
